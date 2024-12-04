cdea7bd61c7259a11196623a6120464e6aeb7cda
ohprettyhak <me@haklee.me>
2024-12-04 15:51:26 +0900

    chore: remove unused files

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,2 @@
-export * from './components/AppsIcon';
-export * from './components/NotificationIcon';
-export * from './components/NotificationActiveIcon';
 export * from './components/BrandIcon';
 export * from './components/SunglassesIcon';
