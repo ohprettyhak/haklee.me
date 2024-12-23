c7c2cfe18ad40fec83ff5a294c6e32e7762dd718
ohprettyhak <me@haklee.me>
2024-12-24 01:24:32 +0900

    chore: change content image url path

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ import { withContentlayer } from 'next-contentlayer';
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+    ],
+  },
+};
 
 export default withVanillaExtract(withContentlayer(nextConfig));
