1369f153bbcfe318f06932e224a615d327cf2fb2
ohprettyhak <me@haklee.me>
2025-01-01 23:02:16 +0900

    fix: change handle back function

--- src/components/BackButton/index.tsx
+++ src/components/BackButton/index.tsx
@@ -8,37 +8,11 @@ import { theme } from '@/styles';
 
 import * as styles from './styles.css';
 
-type PreviousEntry = { idx?: number } | null;
-
 const BackButton: FC<ComponentProps<'button'>> = ({ className, ...props }): ReactElement => {
   const router = useRouter();
 
   const handleBack = () => {
-    const currentHost: string = window.location.origin;
-    const currentPathname: string = window.location.pathname;
-    const previousEntry: PreviousEntry = window.history.state?.idx > 0 && window.history.state;
-
-    const getParentPath = (pathname: string): string => {
-      const segments: string[] = pathname.split('/').filter(Boolean);
-      if (segments.length <= 1) return '/';
-      return `/${segments.slice(0, -1).join('/')}`;
-    };
-
-    const parentPath: string = getParentPath(currentPathname);
-
-    if (previousEntry) {
-      const previousUrl = new URL(document.referrer);
-
-      if (previousUrl.origin !== currentHost) {
-        router.replace(`${currentHost}${parentPath}`);
-      } else if (previousUrl.pathname === currentPathname && previousUrl.hash) {
-        router.replace(`${currentHost}${parentPath}`);
-      } else {
-        router.back();
-      }
-    } else {
-      router.replace(`${currentHost}${parentPath}`);
-    }
+    router.back();
   };
 
   return (
