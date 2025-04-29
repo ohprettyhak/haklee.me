2e1b67d8e1d302d778da75dade816e3aba99db2e
Hak Lee <me@haklee.me>
2025-04-30 00:41:30 +0900

    style: enhance font clarity using optimizeLegibility and font smoothing (#37)

--- packages/style/src/global.css.ts
+++ packages/style/src/global.css.ts
@@ -1,12 +1,16 @@
 import './reset.css';
 
-import { globalStyle,  } from '@vanilla-extract/css';
+import { globalStyle } from '@vanilla-extract/css';
 
 import { theme } from './theme.css';
 
 globalStyle('body', {
   paddingBottom: 'env(safe-area-inset-bottom)',
   fontFamily: theme.fontFamily.sans,
+  textRendering: 'optimizeLegibility',
+  WebkitFontSmoothing: 'antialiased',
+  WebkitTextSizeAdjust: '100%',
+  MozOsxFontSmoothing: 'grayscale',
   overflowX: 'hidden',
   overflowY: 'scroll',
 });
