66835f37d4c374c24a68a1398901497df888cee0
ohprettyhak <me@haklee.me>
2025-06-30 01:27:16 +0900

    feat(site): add custom throttle hooks for improved performance and callback management

--- site/src/hooks/useThrottle.ts
+++ site/src/hooks/useThrottle.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 export const throttle = <T extends any[]>(func: (...args: T) => void, delay: number) => {
   let inThrottle = false;
@@ -9,4 +10,30 @@ export const throttle = <T extends any[]>(func: (...args: T) => void, delay: num
       setTimeout(() => (inThrottle = false), delay);
     }
   };
+};
+
+export const useThrottle = <T>(value: T, delay: number): T => {
+  const [throttledValue, setThrottledValue] = useState<T>(value);
+  const throttledUpdate = useRef(throttle((newValue: T) => setThrottledValue(newValue), delay));
+
+  useEffect(() => {
+    throttledUpdate.current(value);
+  }, [value]);
+
+  return throttledValue;
+};
+
+export const useThrottledCallback = <T extends any[]>(
+  callback: (...args: T) => void,
+  delay: number
+) => {
+  const throttledFunction = useRef(throttle(callback, delay));
+
+  useEffect(() => {
+    throttledFunction.current = throttle(callback, delay);
+  }, [callback, delay]);
+
+  return useCallback((...args: T) => {
+    throttledFunction.current(...args);
+  }, []);
 };
