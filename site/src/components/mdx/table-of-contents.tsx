907b5d78d1ec77bc7ec0964031d350835de55563
ohprettyhak <me@haklee.me>
2025-06-29 14:17:17 +0900

    feat(site): integrate HoverCard component for enhanced table of contents navigation

--- site/src/components/mdx/table-of-contents.tsx
+++ site/src/components/mdx/table-of-contents.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import * as HoverCard from '@radix-ui/react-hover-card';
+import { AnimatePresence, motion } from 'motion/react';
 import { useEffect, useRef, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
@@ -16,6 +18,8 @@ type TableOfContentsProps = {
 export const TableOfContents = ({ toc }: TableOfContentsProps) => {
   const [activeId, setActiveId] = useState<string | null>(toc[0]?.id || null);
   const observerRef = useRef<IntersectionObserver | null>(null);
+
+  const [hoverShow, setHoverShow] = useState<boolean>(false);
 
   useEffect(() => {
     const handleIntersect = (entries: IntersectionObserverEntry[]) => {
@@ -40,21 +44,70 @@ export const TableOfContents = ({ toc }: TableOfContentsProps) => {
   }, [toc, activeId]);
 
   return (
-    <ul className="hidden w-full h-fit list-none gap-[0.25rem] tablet:flex tablet:flex-col">
-      {toc.map(({ id, level, text }) => (
-        <li key={text}>
-          <a
-            href={`#${id}`}
-            className={twMerge(
-              'text-[var(--color-toc)] text-sm text-left break-keep cursor-pointer [transition:color_0.2s_cubic-bezier(0.4,0,0.2,1),font-weight_0.2s_cubic-bezier(0.4,0,0.2,1)] hover:text-[var(--color-text)] hover:font-medium',
-              id === activeId && 'text-[var(--color-text)] font-medium',
+    <HoverCard.Root openDelay={100} open={hoverShow} onOpenChange={setHoverShow}>
+      <HoverCard.Trigger asChild>
+        <ul className="hidden w-full h-fit list-none tablet:flex tablet:flex-col">
+          {toc.map(({ id, level }) => (
+            <li key={id} className="relative inline-block h-[1.5rem]">
+              <button
+                className="h-[0.5rem] rounded-full bg-[var(--color-menu-background)]"
+                style={{ width: `${widthMap[level]}px` }}
+              />
+            </li>
+          ))}
+        </ul>
+      </HoverCard.Trigger>
+      <HoverCard.Portal forceMount>
+        <div>
+          <AnimatePresence>
+            {hoverShow && (
+              <HoverCard.Content side="left" align="start" asChild>
+                <motion.ul
+                  initial={{ opacity: 0, x: 112 }}
+                  animate={{ opacity: 1, x: 96 }}
+                  exit={{ opacity: 0, x: 112, transition: { duration: 0.1 } }}
+                  transition={{ duration: 0.5, type: 'spring' }}
+                  className={twMerge(
+                    'column relative min-w-[8rem] max-h-[calc(100svh-4rem)] px-3 py-1 -my-1',
+                    'rounded-lg border border-[var(--color-toc-border)] bg-[var(--color-toc)] shadow-context-menu',
+                    'scrollbar-none overflow-auto z-10 transform-gpu',
+                  )}
+                >
+                  {toc.map(({ id, level, text }) => (
+                    <li key={text}>
+                      <a
+                        href={`#${id}`}
+                        className={twMerge(
+                          'row-between py-[0.25rem]',
+                          'text-[var(--color-text-secondary)] [transition:color_0.2s_cubic-bezier(0.4,0,0.2,1),font-weight_0.2s_cubic-bezier(0.4,0,0.2,1)] hover:text-[var(--color-text)]',
+                          id === activeId && 'text-[var(--color-text)] font-medium',
+                        )}
+                        style={{ marginLeft: `${(level - 2) * 10}px` }}
+                      >
+                        <span className="text-xs text-left break-keep cursor-pointer hover:font-medium">
+                          {text}
+                        </span>
+                        <span className="ml-[1rem] text-xxxs font-medium uppercase opacity-50">
+                          h{level}
+                        </span>
+                      </a>
+                    </li>
+                  ))}
+                </motion.ul>
+              </HoverCard.Content>
             )}
-            style={{ marginLeft: `${(level - 2) * 10}px` }}
-          >
-            {text}
-          </a>
-        </li>
-      ))}
-    </ul>
+          </AnimatePresence>
+        </div>
+      </HoverCard.Portal>
+    </HoverCard.Root>
   );
+};
+
+const widthMap: Record<number, number> = {
+  1: 72 - 6,
+  2: 60 - 6,
+  3: 48 - 6,
+  4: 36 - 6,
+  5: 24 - 6,
+  6: 12 - 6,
 };
